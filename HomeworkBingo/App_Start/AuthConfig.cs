7a9d60fcf7820e1f9dab5cc7f2a5b2915d33e24c
Lloyd Cledwyn <cledwyn@gmail.com>
2013-06-17 15:48:55 -0500

    Added Google Oauth, and changed homepage

--- HomeworkBingo/App_Start/AuthConfig.cs
+++ HomeworkBingo/App_Start/AuthConfig.cs
@@ -26,7 +26,7 @@ namespace HomeworkBingo
             //    appId: "",
             //    appSecret: "");
 
-            //OAuthWebSecurity.RegisterGoogleClient();
+            OAuthWebSecurity.RegisterGoogleClient();
         }
     }
 }
